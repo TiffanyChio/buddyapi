22fe020b2b283868e57f774269ef307cc9c3cc01
Tiffany Chio <tiffany.chio0801@gmail.com>
2020-01-27 23:10:20 -0600

    changed key name for login key for documentatio purposes

--- config.py
+++ config.py
@@ -2,7 +2,7 @@ import os
 basedir = os.path.abspath(os.path.dirname(__file__))
 
 class Config(object):
-  SECRET_KEY = os.environ.get('LOGIN')
+  SECRET_KEY = os.environ.get('LOGINKEY')
   SQLALCHEMY_DATABASE_URI = os.environ.get('DATABASE_URL') or \
     'sqlite:///' + os.path.join(basedir, 'app.db')
   SQLALCHEMY_TRACK_MODIFICATIONS = False
